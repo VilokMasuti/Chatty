88215026101168383b237cfdba23b9f61baac201
VilokMasuti <119694262+VilokMasuti@users.noreply.github.com>
2024-12-05 18:44:32 +0530

    vite error
    vite error

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,23 +7,26 @@ import cookieParser from "cookie-parser"; // Import cookie-parser
 import cors from 'cors';
 import path from 'path';
 import { app, server } from "./db/socket.js";
+
+
+
 dotenv.config();
 
-// Middleware
-app.use(express.json({ limit: '10mb' })); // Increase payload limit
-app.use(cookieParser()); // Use cookie-parser middlewar
-app.use(cors(
-  {
-    origin: 'http://localhost:5173',
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
     credentials: true,
-  }
+  })
+);
 
-))
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
-
-// Register routes with correct base URL
-app.use('/api/auth', authRoutes);
-app.use('/api/messages', messageRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
@@ -32,12 +35,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-// Connect to the database and start the server
-const PORT = process.env.PORT || 5000;
-const __dirname = path.resolve();
-
-connectDB();
-
 server.listen(PORT, () => {
-  console.log(`Server is running on PORT: ${PORT}`);
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
 });
